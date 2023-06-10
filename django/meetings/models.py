6558dc0ccc71b434462a5fc7fc6ea85d7ed6e114
Lucas Braga <lucasfbraga20@gmail.com>
2023-06-10 17:01:54 -0400

    add metting model

---
+++ django/meetings/models.py
@@ -0,0 +1,28 @@
+"""
+Meeting models.
+"""
+
+import uuid
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class ApprovedMeeting(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    approved = models.BooleanField(verbose_name=_('approved'), default=False)
+    meeting = models.ForeignKey('meetings.Meeting', verbose_name=_('meeting'),
+                                on_delete=models.CASCADE, related_name='meeting_approved')
+    user = models.ForeignKey('users.User', verbose_name=_('user'),
+                             on_delete=models.CASCADE, related_name='approved_meeting')
+
+
+class Meeting(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    description = models.TextField(verbose_name=_('description'))
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      verbose_name=_('created at'))
+    work_step = models.ForeignKey('works.WorkStep', verbose_name=_('work step'),
+                                  on_delete=models.DO_NOTHING, related_name='step_meeting')
+    participants = models.ManyToManyField('users.User', related_name='meeting_participants',
+                                          through='ApprovedMeeting')
--- django/tcc_control/settings.py
+++ django/tcc_control/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS += [
     'timetables',
     'users',
     'works',
+    'meetings',
 ]
 
 MIDDLEWARE = [
