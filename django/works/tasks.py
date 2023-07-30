491261ddf21002811bf4025b1ac3c9c1b298dfdf
Lucas Braga <lucasfbraga20@gmail.com>
2023-07-30 17:40:01 -0400

    add task to send notifications

--- django/works/tasks.py
+++ django/works/tasks.py
@@ -5,11 +5,13 @@ from django.db.models import Q
 from celery import shared_task
 
 from notifications.serializers import NotificationSerializer
+from notifications.utils import send_notification
 from users.models import User
 from timetables.models import Timetable
-from works.models import FinalWork
+from works.models import FinalWork, FinalWorkStage
 
 from core.utils import generate_work_stages, process_stage_status
+from core.defaults import completed_status
 
 
 @shared_task
@@ -34,15 +36,49 @@ def process_final_works():
 
 @shared_task()
 def send_notifications():
-    author = User.objects.get(username='lucas.braga')
-    receiver = User.objects.filter(username='admin')
+    today = datetime.date.today()
+    # today = datetime.date(2023, 6, 26)
 
-    serializer = NotificationSerializer(data={
-        'author': author.id,
-        'receiver': [user.id for user in receiver],
-        'description': 'teste'
-    })
+    tomorrow = datetime.timedelta(days=1) + today
 
-    serializer.is_valid(raise_exception=True)
+    send_to_supervisor = FinalWorkStage.objects.filter(
+        Q(stage__send_date_supervisor=tomorrow)
+    ).exclude(status__in=completed_status)
 
-    instance = serializer.save()
+    for work in send_to_supervisor:
+        receivers = []
+        receivers.append(work.final_work.supervisor)
+        receivers += list(work.final_work.mentees.all())
+        send_notification(
+            description=f'Amanhã é a data de entrega ao supervisor da etapa: "{work.stage.description}"',
+            author=None,
+            receivers=receivers
+        )
+
+    send_date = FinalWorkStage.objects.filter(
+        Q(stage__send_date=tomorrow)
+    ).exclude(status__in=completed_status)
+
+    for work in send_date:
+        receivers = []
+        receivers.append(work.final_work.supervisor)
+        receivers += list(work.final_work.mentees.all())
+        send_notification(
+            description=f'Amanhã é a data de entrega da etapa: "{work.stage.description}"',
+            author=None,
+            receivers=receivers
+        )
+
+    presentation_date = FinalWorkStage.objects.filter(
+        Q(stage__presentation_date=tomorrow)
+    ).exclude(status__in=completed_status)
+
+    for work in presentation_date:
+        receivers = []
+        receivers.append(work.final_work.supervisor)
+        receivers += list(work.final_work.mentees.all())
+        send_notification(
+            description=f'Amanhã é a data de apresentação da etapa: "{work.stage.description}"',
+            author=None,
+            receivers=receivers
+        )
