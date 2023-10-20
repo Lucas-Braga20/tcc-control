78207564ac370235a65d3f0d8934a69ad90056cf
Lucas Braga <lucasfbraga20@gmail.com>
2023-10-19 21:36:55 -0400

    fix timetable template upload

--- django/timetables/documents.py
+++ django/timetables/documents.py
@@ -4,7 +4,11 @@ Contém todas as funções utilitárias para tratar documentos no módulo de
 TimeTable.
 """
 
+from django.utils.text import slugify
+
 
 def get_template_folder(instance, filename):
     """Recupera o caminho do template."""
-    return f'timetables/{instance.id}/templates/${filename}'
+    new_filename = slugify(filename)
+
+    return f'timetables/{instance.id}/templates/${new_filename}'
